9848baaa299e530acaf90d78ebd1e18fd962e755
jameswalker <james.walker@fbinv.com>
2024-05-10 15:38:45 -0600

    Adds unit test for new game

--- crazy-8s/game/game_test.go
+++ crazy-8s/game/game_test.go
@@ -11,8 +11,8 @@ func TestParseStatus(t *testing.T) {
 
 	var tests = []struct {
 		name string
-		input string
-		want  Status
+		given string
+		expected  Status
 	} {
 		{"Pending status", "pending", PENDING},
 		{"In progress status", "in_progress", IN_PROGRESS},
@@ -22,9 +22,26 @@ func TestParseStatus(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			actual, _ := ParseStatus(test.input)
-			assert.Equal(test.want, actual, "Got %s, want %s", actual, test.want)
+			actual, _ := ParseStatus(test.given)
+			assert.Equal(test.expected, actual, "Got %s, want %s", actual, test.expected)
 		})
 	}
 }
 
+func TestNewGame(t *testing.T) {
+	assert := assert.New(t)
+
+	givenOwnerId := "ownerId"
+	givenOwnerName := "TestUser"
+	
+	actual := NewGame(givenOwnerId, givenOwnerName)
+
+	assert.NotNil(actual.GetId())
+	assert.Equal(givenOwnerId, actual.GetOwnerId())
+	assert.Equal(100, actual.GetMaxPoints())
+	assert.Equal(1, len(actual.GetPlayers()))
+	assert.IsType(&StandardDeck{}, actual.GetDeck())
+	assert.Empty(actual.discardPile)
+	assert.Equal(PENDING, actual.GetStatus())
+	assert.Empty(actual.GetCurrentTurn())
+}
